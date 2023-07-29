88aec05066fd8bbf45a180773d67ed2d2002bbce
Oduduabasi Usoro <yakndarausoro1@gmail.com>
2023-07-29 20:57:51 +0100

    created first code example for the exercise using a while loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+let String = "Example";
+const repeatString = function(string, num) {
+    while (string <= 3) {
+        alert(string);
+        string++;
+    }
 };
 
 // Do not edit below this line
