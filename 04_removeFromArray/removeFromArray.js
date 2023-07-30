53848f10bae6eefb39285c46d7b0bfafa01c9665
Oduduabasi Usoro <yakndarausoro1@gmail.com>
2023-07-30 17:58:59 +0100

    used array.from to try and remove 3 from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(removeArr) {
+    removeFromArray = ([1, 2, 3, 4], 3);
+    removeArr.from(removeFromArray.keys(3));
 };
 
 // Do not edit below this line
