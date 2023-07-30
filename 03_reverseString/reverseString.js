8de42c7a1142d9e1910f1a913dba089e4ea77ef3
Oduduabasi Usoro <yakndarausoro1@gmail.com>
2023-07-30 15:50:57 +0100

    added code for reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
 
-};
+    for (i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
+}
+
+reverseString("Odudu");
 
 // Do not edit below this line
 module.exports = reverseString;
