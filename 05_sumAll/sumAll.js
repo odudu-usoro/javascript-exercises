b23f34b1117b6fbe8e36b8523aae863d3725cb1f
Oduduabasi Usoro <yakndarausoro1@gmail.com>
2023-07-31 16:33:18 +0100

    created code syntax for 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function addNumbers(nums) {
+    const numArray = [1, 2, 3, 4, 5];
+    const result = addNumbers(numArray);
+    let finalSum = 0;
 
+    for (i = 0; i < nums.length; i++) {
+        finalSum += nums[i];
+    }
+
+    return finalSum;
 };
+
+return result;
 
 // Do not edit below this line
 module.exports = sumAll;
