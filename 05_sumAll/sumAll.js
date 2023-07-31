2589de1ed5e8af36740c0ee27b60c916eb17dcb8
Oduduabasi Usoro <yakndarausoro1@gmail.com>
2023-07-31 16:35:50 +0100

    testing program with different code syntax

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,31 @@
-const sumAll = function addNumbers(nums) {
-    const numArray = [1, 2, 3, 4, 5];
-    const result = addNumbers(numArray);
-    let finalSum = 0;
+// const sumAll = function addNumbers(nums) {
+//     const numArray = [1, 2, 3, 4, 5];
+//     const result = addNumbers(numArray);
+//     let finalSum = 0;
 
-    for (i = 0; i < nums.length; i++) {
-        finalSum += nums[i];
+//     for (i = 0; i < nums.length; i++) {
+//         finalSum += nums[i];
+//     }
+
+//     return finalSum;
+// };
+
+// return result;
+
+
+function addNumbers(numbers) {
+    let sum = 0;
+  
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
     }
-
-    return finalSum;
-};
-
-return result;
-
+  
+    return sum;
+  }
+  
+  const numbersArray = [1, 2, 3, 4, 5];
+  const result = addNumbers(numbersArray);
+  console.log(result); // Output: 15
+  
 // Do not edit below this line
 module.exports = sumAll;
