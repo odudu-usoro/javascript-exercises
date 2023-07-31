840c3289e28b4cba9c39cc64e817375e1ec6f799
Oduduabasi Usoro <yakndarausoro1@gmail.com>
2023-07-31 18:50:04 +0100

    updated correct code on program exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,18 @@
-const sumAll = function addNumbers(nums) {
-    
-    let finalSum = 0;
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
 
-    for (let i = 0; i < nums.length; i++) {
-        finalSum += nums[i];
-    }
-
-    return finalSum;
+  return sum;
 };
-
-const numArray = [1, 2, 3, 4, 5];
-const result = addNumbers(numArray);
-return result;
-
 
 // Do not edit below this line
 module.exports = sumAll;
