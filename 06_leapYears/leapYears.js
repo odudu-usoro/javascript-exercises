78658397c2b0b5d5e386a8b7adc52c590f449c7b
Oduduabasi Usoro <yakndarausoro1@gmail.com>
2023-08-01 12:05:32 +0100

    deleted miscellanous code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -6,14 +6,6 @@ const leapYears = function(year) {
     }
 };
 
-const isLeapYear = function(year) {
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  
 // Do not edit below this line
 module.exports = leapYears;
  
