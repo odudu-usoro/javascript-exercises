2a1f310eef5fb5878645172a1685f02a05042a50
Oduduabasi Usoro <yakndarausoro1@gmail.com>
2023-08-01 10:05:52 +0100

    Created first solution to the exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    year.prompt() = "Input any given year";
+    if (year % 4 == 0 && year % 400 == 0 || year % 100 == 0){
+        console.log("This is a leap year");
+    } else {
+        console.log("This is not a leap year, Input another year")
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+ 
